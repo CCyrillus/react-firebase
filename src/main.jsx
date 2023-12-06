b013fdc462e533dee3954472d9ff3c39081ccd59
Kir Cherevatyi <cyrillcherevatyi@gmail.com>
2023-12-06 23:01:03 +0200

    vite fix router

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,33 @@ import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
-import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HomePage } from "./Pages/HomePage.jsx";
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>,
+//   document.getElementById('root')
+// )
+const router = createBrowserRouter([
+  {
+    path: "/vite-react-router/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-react-router/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  document.getElementById('root')
-)
+  <React.StrictMode>
+    <RouterProvider router={router} store={store}/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
