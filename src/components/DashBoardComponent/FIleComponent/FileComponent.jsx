6708308141faad36bdd4791e56fe51df94ba9555
Kir Cherevatyi <cyrillcherevatyi@gmail.com>
2023-12-18 15:09:16 +0200

    fix afterl reload page

--- src/components/DashBoardComponent/FIleComponent/FileComponent.jsx
+++ src/components/DashBoardComponent/FIleComponent/FileComponent.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
 
 import Header from "./Header"
-import { useParams } from "react-router-dom"
+import { useNavigation, useParams } from "react-router-dom"
 import { useSelector, shallowEqual } from "react-redux";
 import CodeEditor from "./CodeEditor";
 
 
 const FileComponent = () => {
 
+    const navigate = useNavigation();
     const { fileId } = useParams();
     const [fileData, setFileData] = useState("");
 
-    const [prevFileData, setPrevFileData]= useState("");
+    const [prevFileData, setPrevFileData] = useState("");
 
-    const { currentFile } = useSelector((state) => ({
+    const { currentFile, isAuthenticated } = useSelector((state) => ({
+        isAuthenticated: state.auth.isAuthenticated,
         currentFile: state.filefolders.userFiles.find(
             (file) => file.docId === fileId
         ),
@@ -22,27 +24,36 @@ const FileComponent = () => {
     );
 
     useEffect(() => {
-        if (currentFile) {
-            setFileData(currentFile.data.data);
-            setPrevFileData(currentFile.data.data)
+        if (!isAuthenticated) {
+            navigate("/react-firebase/")
         }
-    }, [currentFile])
+    }, [isAuthenticated])
 
-    return (
-        <div>
-            <Header
-                fileName={currentFile.data.name}
-                fileData={fileData}
-                prevFileData={prevFileData}
-                fileId={fileId}
-            />
-            <CodeEditor
-                fileName={currentFile.data.name}
-                data={fileData}
-                setData={setFileData}
-            />
-        </div>
-    )
+
+    useEffect(() => {
+        if (currentFile) {
+            setFileData(currentFile?.data?.data);
+            setPrevFileData(currentFile?.data?.data)
+        }
+    }, [currentFile, currentFile?.data?.data])
+
+    if (isAuthenticated)
+        return (
+            <div>
+                <Header
+                    fileName={currentFile?.data?.data}
+                    fileData={fileData}
+                    prevFileData={prevFileData}
+                    fileId={fileId}
+                />
+                <CodeEditor
+                    fileName={currentFile?.data?.data}
+                    data={fileData}
+                    setData={setFileData}
+                />
+            </div>
+        )
+    return<div>Login First</div>
 }
 
 export default FileComponent
